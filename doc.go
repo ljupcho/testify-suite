11a5214745f792285034bf66343496ec5f66cd63
Ljupcho Apostolov <ljupcho.apostolov@3fs.si>
2022-03-30 21:00:48 +0200

    add package doc

---
+++ doc.go
@@ -0,0 +1,8 @@
+// The suite package provides a basic structure for using structs as testing suites, and methods on those structs as tests.  It includes setup/teardown functionality in the way of interfaces.
+package testifysuite
+
+// blank imports help docs.
+import (
+	// suite package
+	_ "github.com/ljupcho/testify-suite/suite"
+)
